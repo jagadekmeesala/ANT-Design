992562e3da836c371076622ea1ccb30669bb78fb
JAGADEK V.V.S.D. MEESALA <85881386+jagadekmeesala@users.noreply.github.com>
2024-03-08 16:02:28 +0530

    Add files via upload

---
+++ Components/Footer/index.js
@@ -0,0 +1,11 @@
+import { Typography } from "antd";
+
+function Footer() {
+    return(
+    <div className ="Footer">
+        
+        <Typography.Link> Developed By Jagadek             Copyright@2024 </Typography.Link>
+         </div>
+    );
+}
+export default Footer;
